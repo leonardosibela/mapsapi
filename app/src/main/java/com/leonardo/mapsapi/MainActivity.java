e5bc535c23fda29d524f548c1adc06038f99df9e
leonardosibela <leonardo_sibela@hotmail.com>
2016-12-27 16:26:56 -0200

    Dev

--- app/src/main/java/com/leonardo/mapsapi/MainActivity.java
+++ app/src/main/java/com/leonardo/mapsapi/MainActivity.java
@@ -1,13 +1,43 @@
 package com.leonardo.mapsapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.latitude)
+    EditText latitude;
+
+    @BindView(R.id.longitude)
+    EditText longitude;
+
+    @BindView(R.id.place_name)
+    TextView placeName;
+
+    @BindView(R.id.distance)
+    TextView distance;
+
+    @BindView(R.id.duration)
+    EditText duration;
+
+    @BindView(R.id.get_info_button)
+    Button getInfoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @OnClick(R.id.get_info_button)
+    public void onGetInfoClicked(Button button) {
+        button.setClickable(false);
+        
     }
 }
